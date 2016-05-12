0eb4484404ca848c85b006aa9d865c0c92e7458e
Nat Lund <natjlund@gmail.com>
2016-05-12 15:56:46 +0100

    Built simple Montecarlo algorithm to replace genetic algorithms.

---
+++ montecarlo.py
@@ -0,0 +1,83 @@
+#!usr/bin/python
+
+import networkx as nx
+import matplotlib.pyplot as plt
+import random
+from graph_geometry import *
+
+# Montecarlo algorithm to supplant the asexually reproducing
+# genetic algorithm.  It should be much simpler, since it won't have
+# all the machinery of genetic algorithms.
+
+# Uses tuple comparison:  It is true that (1,3) < (2,2)
+
+
+def run_simulation(G, seed):
+    
+    layout, fitness = montecarlo(G, seed, count_crosses, no_crosses, mutation_rate=0.25, n=100)  # First minimise edge crossings
+    layout, fitness = montecarlo(G, layout, calc_fitness, dummy, mutation_rate=0.2, n=100)  # Then prettify
+    
+    print 'Final fitness', fitness
+    nx.draw(G, layout)
+    plt.show()
+    
+    
+def montecarlo(G, seed, fitness_function, break_function, mutation_rate=0.1, n=10):
+    
+    layout = seed.copy()
+    fitness = fitness_function(G, layout)
+    best_fitness = fitness
+    
+    print 'Initial fitness', best_fitness
+    nx.draw(G, layout)
+    plt.show()
+    
+    for k in range(n):
+            
+        if break_function(fitness):
+            break
+
+        new_layout = layout.copy()
+        
+        for node in G.nodes():
+            if random.random() < mutation_rate:
+                #print 'mutating'
+                new_layout[node] = random.random(), random.random()
+                
+        fitness = fitness_function(G, new_layout)
+        print 'Iterated, fitness is now:', fitness
+        
+        if fitness < best_fitness: # Improvement in fitness
+            layout = new_layout
+            best_fitness = fitness
+            print 'Fitness improved', best_fitness
+            nx.draw(G, layout)
+            plt.show()
+    
+    print 'Montecarlo done, fitness is:', fitness_function(G, layout)
+    print
+    
+    return layout, best_fitness
+        
+        
+def calc_fitness(G, layout):
+    return count_crosses(G, layout), total_node_on_edge_penalty(G, layout, 0.2)
+
+
+def no_crosses(fitness):
+    if fitness == 0:
+        return True
+    else:
+        return False
+
+def dummy(fitness):
+    return False
+
+if __name__ == '__main__':
+    size = 8
+    G = nx.gnm_random_graph(size,11)
+    seed = nx.spring_layout(G)
+    
+    layout = run_simulation(G, seed)
+
+    print (1,3) < (2,2)
