947698eb95962f7325e38e3b2e1e4bbedd804e0b
Nat Lund <natjlund@gmail.com>
2016-05-12 12:43:03 +0100

    Added new main file to project.

---
+++ disentangle.py
@@ -0,0 +1,64 @@
+#!usr/bin/python
+
+import networkx as nx
+from evol_nx_layout import evolve_asexually
+
+"""
+This the main file for the disentangler project.
+
+The purpose of the project is to find algorithms to draw a two-dimensional
+network graph (of nodes and edges) in a nice way.  That is, with minimal
+edge crossings, maximum symmetry, and in general in a clean, easy-to-understand
+way.
+
+The disentangler project is a work in progress.  The current state is good,
+but not perfect.  For simple cases, a graph layout is produced that has
+no edge crossings, no nodes lying stupidly close to edges, and no absurdly
+long edges.  But it can still look a bit weird, not quite right.
+Experiments continue...
+
+The project holds a lot of dead ends - strategies that were tried
+and found not to be fruitful.  Therefore, there is a lot of code that is
+essentially dead - it will never be used again, but is kept as a record
+of experiments that were done.
+
+The best approach discovered so far is an essentially random approach -
+move nodes around at random and see if it makes it better.  This started
+out as a genetic algorithm, with sexual reproduction, and a fitness function.
+It was discovered that ASEXUAL reproduction gave better results.
+An asexual reproduction algorithm is almost certainly equivalent to some 
+kind of Montecarlo algorithm.  Therefore, the current best algorithm will
+be replicated in new Montecarlo algorithm, which should be much simpler
+without all the inherited machinery for genetic algorithms.
+"""
+
+def elegant_layout(G):
+    '''
+    Given networkx graph G, return a nice elegant layout.
+    '''
+    seed = nx.spring_layout(G)
+    
+    return evolve_asexually(G,seed)
+    
+    
+def elegantify_layout(G, seed):
+    '''
+    Given networkx graph G and a layout, attempt to find an improvement
+    to the layout and return it.
+    '''
+    return evolve_asexually(G,seed)
+    
+    
+def demo():
+    '''
+    Run a demo showing how a random, ugly network graph evolves into
+    a much nicer-looking graph.
+    '''
+    size = 8
+    G = nx.gnm_random_graph(size,11)
+    seed = nx.spring_layout(G)
+    evol = evolve_asexually(G,seed)
+    
+    
+if __name__ == '__main__':
+    demo()
